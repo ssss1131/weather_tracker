d78f7e6240f267542bea8519ef6bba1795207eac
ssss1131 <sauletmardan20@gmail.com>
2024-11-08 16:11:11 +0500

    feat: add exception handler for attempt to creating account with already existing username

---
+++ src/main/java/com/ssss/weather_tracker/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.ssss.weather_tracker.exception;
+
+import com.ssss.weather_tracker.dto.request.UserRegistrationDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ModelAndView handleUserAlreadyExists(UserAlreadyExistsException ex){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("errorMessage", ex.getMessage());
+        mav.addObject("user", new UserRegistrationDto());
+        mav.setViewName("/auth/register");
+        return mav;
+    }
+//
+//    @ExceptionHandler(value = Exception.class)
+//    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+//        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+//            throw e;
+//        }
+////        logger.error("Request URL: " + req.getRequestURL() + " raised " + e);
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("exception", e);
+//        mav.addObject("url", req.getRequestURL());
+//        mav.setViewName("/error/error");
+//        return mav;
+//    }
+}

db4751471dcfc2ff19e0d06859d282db1cac563f
ssss1131 <sauletmardan20@gmail.com>
2024-11-18 14:53:59 +0500

    feat: add handler for most of handmade exceptions, and for 404 and all others add handler

--- src/main/java/com/ssss/weather_tracker/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/ssss/weather_tracker/exception/GlobalControllerExceptionHandler.java
@@ -2,44 +2,66 @@ package com.ssss.weather_tracker.exception;
 
 import com.ssss.weather_tracker.dto.request.UserLoginDto;
 import com.ssss.weather_tracker.dto.request.UserRegistrationDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import static com.ssss.weather_tracker.util.Constants.LOGIN_VIEW;
-import static com.ssss.weather_tracker.util.Constants.REGISTER_VIEW;
+import static com.ssss.weather_tracker.util.Constants.*;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ModelAndView handleUserAlreadyExists(UserAlreadyExistsException ex){
         ModelAndView mav = new ModelAndView();
-        mav.addObject("errorMessage", ex.getMessage());
-        mav.addObject("user", new UserRegistrationDto());
+        mav.addObject(ERROR_MESSAGE_ATTRIBUTE, ex.getMessage());
+        mav.addObject(USER_ATTRIBUTE, new UserRegistrationDto());
         mav.setViewName(REGISTER_VIEW);
         return mav;
     }
 
     @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ModelAndView handleAuthenticationException(AuthenticationException ex){
         ModelAndView mav = new ModelAndView();
-        mav.addObject("errorMessage", ex.getMessage());
-        mav.addObject("user", new UserLoginDto());
+        mav.addObject(ERROR_MESSAGE_ATTRIBUTE, ex.getMessage());
+        mav.addObject(USER_ATTRIBUTE, new UserLoginDto());
         mav.setViewName(LOGIN_VIEW);
         return mav;
     }
-//
-//    @ExceptionHandler(value = Exception.class)
-//    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-//        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
-//            throw e;
-//        }
-////        logger.error("Request URL: " + req.getRequestURL() + " raised " + e);
-//        ModelAndView mav = new ModelAndView();
-//        mav.addObject("exception", e);
-//        mav.addObject("url", req.getRequestURL());
-//        mav.setViewName("/error/error");
-//        return mav;
-//    }
+
+    @ExceptionHandler(AlreadySavedLocationException.class)
+    public String handleAlreadySavedLocationException(AlreadySavedLocationException ex,
+                                                            HttpServletRequest request,
+                                                            RedirectAttributes redirectAttributes
+                                                            ){
+        String query = request.getParameter(QUERY_ATTRIBUTE);
+        redirectAttributes.addFlashAttribute(ERROR_MESSAGE_ATTRIBUTE, ex.getMessage());
+        return "redirect:/search?query=" + query;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handle404(Exception ex) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName(ERROR_404_VIEW);
+        mav.addObject(ERROR_MESSAGE_ATTRIBUTE, "The page you are looking for was not found.");
+        return mav;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView handle500(Exception ex) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName(ERROR_500_VIEW);
+        mav.addObject(ERROR_MESSAGE_ATTRIBUTE, "An unexpected error occurred.");
+        return mav;
+    }
+
 }

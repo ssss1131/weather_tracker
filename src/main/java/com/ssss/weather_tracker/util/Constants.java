956794fa666c86c4c3925a8113f66ffc210a0547
ssss1131 <sauletmardan20@gmail.com>
2024-11-18 14:49:11 +0500

    feat:add constants for endpoints, views, attributes, redirect paths, and paths of open weathep api service

--- src/main/java/com/ssss/weather_tracker/util/Constants.java
+++ src/main/java/com/ssss/weather_tracker/util/Constants.java
@@ -10,13 +10,29 @@ public class Constants {
     public static final String AUTH_LOGIN = AUTH_BASE + LOGIN;
     public static final String AUTH_REGISTER = AUTH_BASE + REGISTER;
     public static final String HOME = "/home";
+    public static final String SEARCH = "/search";
+    public static final String SIGN_OUT ="/logout";
+    public static final String DELETE_LOCATION = "locations/delete/{id}";
+    public static final String ADD_LOCATION = "locations/add";
 
     public static final String LOGIN_VIEW = "/auth/login";
     public static final String REGISTER_VIEW = "/auth/register";
     public static final String HOME_VIEW = "/main/home";
+    public static final String SEARCH_RESULT_VIEW = "/main/search";
+    public static final String ERROR_404_VIEW = "/error/404";
+    public static final String ERROR_500_VIEW = "/error/500";
 
     public static final String USER_ATTRIBUTE = "user";
-    public static final String REDIRECT_HOME = "redirect:/home";
+    public static final String USER_ID_ATTRIBUTE = "userId";
+    public static final String USER_NAME_ATTRIBUTE = "userName";
+    public static final String ERROR_MESSAGE_ATTRIBUTE = "errorMessage";
+    public static final String LOCATION_ATTRIBUTE = "locations";
+    public static final String QUERY_ATTRIBUTE = "query";
+
+    private static final String REDIRECT = "redirect:";
+    public static final String REDIRECT_HOME = REDIRECT + HOME;
+    public static final String REDIRECT_LOGIN = REDIRECT + AUTH_LOGIN;
+    public static final String REDIRECT_SEARCH = REDIRECT + SEARCH + "?query=";
 
     public static final String DIRECT_PATH = "/geo/1.0/direct";
     public static final String WEATHER_PATH = "/data/2.5/weather";
